a090e744442a9351484e694c6b963cd70302194e
Emanuel <emanuel.garrido@hotmail.com>
2021-09-30 21:39:41 -0300

    Criando a pasta core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { StarComponent } from './shared/component/star/star.component';
-import { ReplacePipe } from './shared/pipe/replace.pipe';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { Error404Component } from './error404/error-404.component';
 import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     Error404Component,
   ],
   imports: [
     BrowserModule,    
     HttpClientModule,
     CourseModule,
+    CoreModule,
     RouterModule.forRoot([      
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
