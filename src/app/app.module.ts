c8ec474e31cd86f4814d293273646e50e157d73f
Emanuel <emanuel.garrido@hotmail.com>
2021-09-30 21:21:21 -0300

    Segregando courses em módulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,36 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from './star/star.component';
 import { ReplacePipe } from './pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { Error404Component } from './error404/error-404.component';
-import { CourseInfoComponent } from './courses/course-info.component';
+import { CourseModule } from './courses/course.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
     NavBarComponent,
     Error404Component,
-    CourseInfoComponent,
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
+    BrowserModule,    
     HttpClientModule,
-    RouterModule.forRoot([
-      {
-        path: 'courses', component: CourseListComponent
-      },
-      {
-        path: 'courses/info/:id', component: CourseInfoComponent
-      },
+    CourseModule,
+    RouterModule.forRoot([      
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
       },
